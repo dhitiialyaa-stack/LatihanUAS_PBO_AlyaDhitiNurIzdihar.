d57853729c534427a92a1e2986c404fef2b14219
alya <dhitiialyaa@gmail.com>
2026-06-22 08:57:38 +0700

    [Tahap 2] Mengubah file database SQL

--- db_latihanuas_pbo_ti1c_alyadhitinurizdihar.sql
+++ db_latihanuas_pbo_ti1c_alyadhitinurizdihar.sql
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 -- Database: `db_latihanuas_pbo_ti1c_alyadhitinurizdihar`
 --
 
--- --------------------------------------------------------
+-- -------------------------------------------------------
 
 --
 -- Table structure for table `tabel_pendaftaran`
